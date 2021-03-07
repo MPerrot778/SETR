8b411c51f227319468f703acc40826f162cedd0b
setr <setr@localhost.localdomain>
2021-03-07 18:59:44 -0500

    update initEcrivain

--- Laboratoire_3/src/commMemoirePartagee.c
+++ Laboratoire_3/src/commMemoirePartagee.c
@@ -10,33 +10,51 @@
 #include <string.h>
 #include "commMemoirePartagee.h"
 
-int initMemoirePartageeLecteur(const char* identifiant, struct memPartage *zone){
-    int shm_fd; // shared memory file descriptor
-    void* shm_ptr;  // points to shared memory
-
-    shm_fd = shm_open(identifiant, O_CREAT | O_RDONLY | O_EXCL);
-
-    if (shm_fd == -1) {
-        perror("shm_open");
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", shm_fd);
-    if (ftruncate(identifiant, sizeof(memPartage)) == -1) {
-        perror("ftruncate");
-        exit(EXIT_FAILURE);
-    }
-
-    shm_ptr = nmap(NULL, sizeof(memPartage), PROT_READ, MAP_SHARED, shm_fd, 0);
-    printf("%d\n" shm_ptr);
-
-    if (shm_ptr == MAP_FAILED) {
-        perror("nmap");
-        exit(EXIT_FAILURE);
-    }
+int initMemoirePartageeLecteur(const char* identifiant, struct memPartage *zone){   
+    
 
 }
 
-int initMemoirePartageeEcrivain(const char* identifiant, struct memPartage *zone, size_t taille, struct memPartageHeader* headerInfos){
+int initMemoirePartageeEcrivain(const char* identifiant, struct memPartage* zone, size_t taille, struct memPartageHeader* headerInfos){
+
+    /* Create shared memory*/
+
+    int shm_fd; // shared memory file descriptor
+    void* shm_ptr;  // points to shared memory
+
+    shm_fd = shm_open(identifiant, O_CREAT | O_RDWR | O_EXCL, 0666);
+
+    if (shm_fd == -1) {
+        perror("shm_open");
+        exit(1);
+    }
+    printf("%d\n", shm_fd);
+    if (ftruncate(identifiant, taille) == -1) {
+        perror("ftruncate");
+        exit(1);
+    }
+
+    shm_ptr = nmap(NULL, taille, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    printf("%d\n", shm_ptr);
+
+    if (shm_ptr == MAP_FAILED) {
+        perror("nmap");
+        exit(1);
+    }
+
+    zone->fd = shm_fd;
+    zone->tailleDonnees = taille;
+
+    /* Create Mutex in shared memory header */
+
+    pthread_mutex_t shm_mutex = PTHREAD_MUTEX_INITIALIZER;
+    if(pthread_mutex_init(&shm_mutex, NULL) != 0) {
+        perror("mutex");
+        exit(1);
+    }
+
+    headerInfos->mutex = shm_mutex;
+    zone->copieCompteur++; 
 
 }
 
