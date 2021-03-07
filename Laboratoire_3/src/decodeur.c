bf7674ce820a930650bdf933b445c0acab838158
Mikaël Perrot <mik.perrot778@outlook.fr>
2021-03-07 16:59:41 -0500

    getopt of decodeur.c started

--- Laboratoire_3/src/decodeur.c
+++ Laboratoire_3/src/decodeur.c
@@ -47,5 +47,42 @@ int main(int argc, char* argv[]){
     // pour décoder une image JPEG contenue dans un buffer!
     // N'oubliez pas également que ce décodeur doit lire les fichiers ULV EN BOUCLE
 
+    int aflag = 0;
+    int bflag = 0;
+    int sflag = 0;
+    int dflag = 0;
+    char *cvalue, *svalue, *dvalue = NULL;
+    int index;
+    int c;
+
+    // -s, -d, --debug
+    while ((c = getopt (argc, argv, "as:d::")) != -1){
+        switch (c){
+            case 'a':
+                aflag = 1;
+                break;
+            case 'b':
+                bflag = 1;
+                break;
+            case 'c':
+                cvalue = optarg;
+                break;
+            case '?':
+                if (optopt == 'c')
+                fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint (optopt))
+                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                fprintf (stderr,
+                        "Unknown option character `\\x%x'.\n",
+                        optopt);
+                return 1;
+            default:
+                abort ();
+        }
+    }
+    for (index = optind; index < argc; index++)
+        printf ("Non-option argument %s\n", argv[index]);
+
     return 0;
 }
